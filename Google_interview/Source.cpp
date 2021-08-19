13718282e1aff3871e3426b9d9869838d60fdc27
iliyn_marinov <ili.2@abv.bg>
2021-08-19 15:07:26 +0300

    Third_commit

--- Google_interview/Source.cpp
+++ Google_interview/Source.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 void Del_matrix(int** matrix, const int rows, const int colomns);
+bool IsBorder(const int const ** matrix, const int i, const int j, const int rows, const int colomns);
 
 int main()
 {
@@ -40,12 +41,30 @@ int main()
 		for (size_t j = 0; j < colomns; j++)
 		{
 			std::cin >> matrix[i][j];
-			if (matrix[i][j] != 0 || matrix[i][j] != 1)
+			if (matrix[i][j] != 0 && matrix[i][j] != 1)
 			{
 				Del_matrix(matrix, rows, colomns);
 				return 1;
 			}
 		}
+	}
+	for (size_t i = 1; i < rows - 1; i++)
+	{
+		for (size_t j = 1; j < colomns - 1; j++)
+		{
+			if (matrix[i][j] == 1 && !IsBorder(matrix, i, j, rows, colomns))
+			{
+				matrix[i][j] = 0;
+			}
+		}
+	}
+	for (size_t i = 0; i < rows; i++)
+	{
+		for (size_t j = 0; j < colomns; j++)
+		{
+			std::cout << matrix[i][j] << ' ';
+		}
+		std::cout << std::endl;
 	}
 
 	Del_matrix(matrix, rows, colomns);
@@ -59,4 +78,24 @@ void Del_matrix(int** matrix, const int rows, const int colomns)
 		delete[]matrix[i];
 	}
 	delete[]matrix;
+}
+bool IsBorder(const int const ** matrix, const int i, const int j, const int rows, const int colomns)
+{
+	if (matrix[i - 1][j] == 1 && IsBorder(matrix, i - 1, j, rows, colomns))
+	{
+		return true;
+	}
+	if (matrix[i][j + 1] == 1 && IsBorder(matrix, i, j + 1, rows, colomns))
+	{
+		return true;
+	}
+	if (matrix[i + 1][j] == 1 && IsBorder(matrix, i + 1, j, rows, colomns))
+	{
+		return true;
+	}
+	if (matrix[i][j - 1] == 1 && IsBorder(matrix, i, j - 1, rows, colomns))
+	{
+		return true;
+	}
+	return false;
 }
